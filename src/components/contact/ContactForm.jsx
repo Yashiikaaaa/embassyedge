9dec78cff3af3abf2cf00f36219f1a3389449731
yashiikaaaa <yashika@truestate.in>
2025-08-20 17:20:48 +0530

    ph number change

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -232,9 +232,9 @@ const ContactForm = ({ contactmodal, setContactModal, leadSource }) => {
               </div>
               <div className="mx-auto max-w-sm w-full">
                 <button className="text-white my-5 p-2 w-full bg-PrestigeBrown flex items-center justify-center hover:bg-opacity-90 transition">
-                  <a href="tel:+919353329893" className="flex items-center">
+                  <a href="tel:+916364254487" className="flex items-center">
                     <Phone className="w-5 h-5 mr-2" />
-                    93533 29893
+                   63642 54487
                   </a>
                 </button>
               </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -124,10 +124,10 @@ export const Navbar = ({ sitevisitmodal, setSiteVisitModal, setContactModal }) =
           {/* Call Button */}
           <div className="hidden lg:flex items-center">
             <a
-              href="tel:+919353329893"
+              href="tel:+916364254487"
               className="flex items-center bg-PrestigeBrown text-white px-4 py-2 rounded-lg shadow-lg hover:bg-opacity-90 transition"
             >
-              <Phone className="w-5 h-5 mr-2" />93533 29893
+              <Phone className="w-5 h-5 mr-2" />63642 54487
             </a>
           </div>
         </div>
