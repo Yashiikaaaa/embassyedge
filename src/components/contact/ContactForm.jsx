07e4249a14e4782625c8cac6829fe21aae7b9965
yashiikaaaa <yashika@truestate.in>
2025-08-29 18:59:10 +0530

    contact form ink

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -126,7 +126,7 @@ const handleSubmit = async (e) => {
 
     try {
       const response = await fetch(
-        "https://googleleadsserver.onrender.com/handleMultipleCampaignData",
+        "https://google-campaign-leads-service-dot-iqol-crm.uc.r.appspot.com/handleMultipleCampaignData",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
