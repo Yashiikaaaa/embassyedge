095fa104827c48a68c825786e1a5487a2be8e7ae
Yashiikaaaa <yashikagoyal0808@gmail.com>
2026-05-04 19:43:43 +0530

    Update features data with corrected values

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -4,9 +4,9 @@ export const Features = () => {
   // Array containing the features data
   const featuresData = [
     { id: 1, title: 'Embassy Green Shore', value: '14 Acres' },
-    { id: 2, title: 'Possession', value: '2029 ' },
-    { id: 3, title: 'Starting Price', value: 'Rs 1.95 Cr*' },
-    { id: 4, title: 'Units', value: '3 BHK' },
+    { id: 2, title: 'Possession', value: ' Dec 2029 ' },
+    { id: 3, title: 'Starting Price', value: '₹ 1.95 Cr*' },
+    { id: 4, title: 'Units', value: '3 & 4 BHK' },
   ];
 
   return (
